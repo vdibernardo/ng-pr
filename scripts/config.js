f29b48e6855cc9f71ad023f9f163b80f97656a3b
vdibernardo <124002599+vdibernardo@users.noreply.github.com>
2025-04-16 17:07:54 -0400

    Created navbar and added custom colors

---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                grape_soda: '#EBEEFF';
+                lavender_haze: '#E2B4FF',
+                cereal_milk: '#FDFAF1'
+                tomato_jam: '#F45624',
+                limeade: '#D6F74C',
+                coltrane_blue: '#7669E9',
+                bloobs: '#531AF4',
+            }
+        }
+    }
+}
